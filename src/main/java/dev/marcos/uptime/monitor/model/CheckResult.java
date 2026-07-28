49db80213edd9448dcb6485d8eb699db0598514c
Marcos <marcosv.java@gmail.com>
2026-07-28 16:44:43 -0400

    feat: new entity check resul

---
+++ src/main/java/dev/marcos/uptime/monitor/model/CheckResult.java
@@ -0,0 +1,32 @@
+package dev.marcos.uptime.monitor.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_check_result")
+@Getter@Setter
+@NoArgsConstructor
+public class CheckResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false,name = "monitor_id")
+    private Monitor monitor;
+    @Column(name = "checked_at",nullable = false)
+    private Instant checkedAt;
+    @Column(name = "http_status")
+    private Integer httpStatus;
+    @Column(name = "response_time_ms",nullable = false)
+    private Long responseTimeMs;
+    @Column(nullable = false)
+    private Boolean success;
+    @Column(name = "error_message")
+    private String errorMessage;
+
+}
