08605af2ed2fa7a9c116bc94f2e15e919f9c8e24
Marcos <marcosv.java@gmail.com>
2026-07-28 14:18:58 -0400

    first commit

---
+++ src/test/java/dev/marcos/uptime/monitor/UptimeMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcos.uptime.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
