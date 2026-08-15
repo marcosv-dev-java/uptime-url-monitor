c1da3bd66376e6b360c9500303dbed5b61cbb35e
Marcos <marcosv.java@gmail.com>
2026-08-15 10:59:47 -0400

    feat(AdminService): log.warn to username not found

--- src/main/java/dev/marcos/uptime/monitor/service/AdminService.java
+++ src/main/java/dev/marcos/uptime/monitor/service/AdminService.java
@@ -4,6 +4,7 @@ import dev.marcos.uptime.monitor.model.Monitor;
 import dev.marcos.uptime.monitor.model.User;
 import dev.marcos.uptime.monitor.repository.MonitorRepository;
 import dev.marcos.uptime.monitor.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 
 @Service
+@Slf4j
 public class AdminService {
     private final MonitorRepository monitorRepository;
     private final UserRepository userRepository;
@@ -22,8 +24,14 @@ public class AdminService {
     }
 
     public List<MonitorSummaryResponse> getAllMonitorsOfUser(String username){
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
+        User user;
+        try {
+             user = userRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
+        }catch (UsernameNotFoundException e){
+            log.warn("User {} not found", username);
+            throw  e;
+        }
         List<Monitor> monitors = monitorRepository.findMonitorByOwner(user);
         List<MonitorSummaryResponse> responses = new ArrayList<>();
         for (Monitor monitor : monitors) {
